6b13c939c18d11eb29ae8b9dbd8f36342866e9f8
Rovic Castro <193394253+roviccastro@users.noreply.github.com>
2025-01-22 18:37:33 +0800

    Create 256 square and a container

---
+++ script.js
@@ -0,0 +1,13 @@
+const mainContainer = document.createElement("div");
+mainContainer.setAttribute("id", "container");
+document.body.appendChild(mainContainer);
+
+function createSquare(){
+  let square = document.createElement("div");
+  square.classList.toggle("square");
+  mainContainer.appendChild(square);
+}
+
+for (let a = 0; a <= 256; a++){
+  createSquare()
+}
